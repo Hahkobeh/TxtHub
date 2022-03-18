92cd8a5c86503c56d4903823d65b3a0e063fa611
JVanderstoop <J.vanderstoop@outlook.com>
2022-03-17 19:19:29 -0600

    UserSystem Testing

--- usersystem/src/test/java/com/txthub/usersystem/UsersystemApplicationTests.java
+++ usersystem/src/test/java/com/txthub/usersystem/UsersystemApplicationTests.java
@@ -3,6 +3,9 @@ package com.txthub.usersystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class UsersystemApplicationTests {
 
