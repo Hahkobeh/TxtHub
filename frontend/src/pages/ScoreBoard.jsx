26af7f6b97d2a6c4c9093982020fee74d761d57f
colin <colin.christophe@ucalgary.ca>
2022-03-23 22:34:37 -0700

    loading for leaderboard

--- frontend/src/pages/ScoreBoard.jsx
+++ frontend/src/pages/ScoreBoard.jsx
@@ -6,6 +6,8 @@ import './ScoreBoard.scss';
 
 function ScoreBoard(){
 
+    const [loading, setLoading] = useState(true);
+
     const [wordleRankings, setWordleRankings] = useState([]);
     const [anagramRankings, setAnagramRankings] = useState([]);
     const [tbaRankings, setTbaRankings] = useState([]);
@@ -14,6 +16,8 @@ function ScoreBoard(){
     const [userAnagramRank, setUserAnagramRank] = useState([]);
 
     const currentName = localStorage.getItem('username');
+    
+    
     SetUp();
 
     async function SetUp(){
@@ -55,119 +59,193 @@ function ScoreBoard(){
                 setTbaRankings(res.data);
         });
 
+        setLoading(false);
+
 
     }
 
-    return(
+    if(loading){
+        return(
 
-        <div>
-            <Layout/>
-
-            <h1 className='score-header'>Leaderboard</h1>
-
-           
-            <ul className = 'leaderboard-container'>
-
-                
-                <li className ='wordle-leaderboard'>
+            <div>
+                <Layout/>
+    
+                <h1 className='score-header'>Leaderboard</h1>
+    
+               
+                <ul className = 'leaderboard-container'>
+    
                     
-                    <h1 className='wordle-header'>Wordle</h1>
-                    <hr className="hr"/>
+                    <li className ='wordle-leaderboard'>
+                        
+                        <h1 className='wordle-header'>Wordle</h1>
+                        <hr className="hr"/>
+                        
+                        <div className ='labels'>
+                            <ul>
+                                <li><h3>Rank</h3></li>
+                                <li><h3>Username</h3></li>
+                                <li><h3>Rating</h3></li>
+                            </ul>
+                                
+                        </div>
+                    </li >
+    
+                    <li className ='anagram-leaderboard'>
+                        
+                        <h1 className='anagram-header'>Anagram</h1>
+                        <hr className="hr"/>
+                        
+    
+                        <div className ='labels'>
+                            <ul>
+                                <li><h3>Rank</h3></li>
+                                <li><h3>Username</h3></li>
+                                <li><h3>Rating</h3></li>
+                            </ul>
+                         
+                        </div>
+                        
+                    </li>
+    
+                    <li className ='tba-leaderboard'>
+                        
+                        <h1 className='tba-header'>TBA</h1>
+                        <hr className="hr"/>
+                        
+    
+                        <div className ='labels'>
+                            <ul>
+                                <li><h3>Rank</h3></li>
+                                <li><h3>Story Name</h3></li>
+                                <li><h3>Rating</h3></li>
+                            </ul>
                     
-                    <div className ='labels'>
-                        <ul>
-                            <li><h3>Rank</h3></li>
-                            <li><h3>Username</h3></li>
-                            <li><h3>Rating</h3></li>
-                        </ul>
+                        </div>
+                        
+                    </li>
+                </ul>
+               
+              
+            </div>
+            
+    
+        );
 
-                            {wordleRankings.map(function(wordleRanking){
-                                return <div>
-                                    <ul className = 'user-score'>
-                                        <li>{wordleRanking.rank}</li>
-                                        <li>{wordleRanking.username}</li>
-                                        <li>{parseInt(wordleRanking.rating)}</li>
-                                    </ul>
-                                    <hr/>
-                                </div>
+    }else{
+        return(
+
+            <div>
+                <Layout/>
+    
+                <h1 className='score-header'>Leaderboard</h1>
+    
+               
+                <ul className = 'leaderboard-container'>
+    
+                    
+                    <li className ='wordle-leaderboard'>
+                        
+                        <h1 className='wordle-header'>Wordle</h1>
+                        <hr className="hr"/>
+                        
+                        <div className ='labels'>
+                            <ul>
+                                <li><h3>Rank</h3></li>
+                                <li><h3>Username</h3></li>
+                                <li><h3>Rating</h3></li>
+                            </ul>
+    
+                                {wordleRankings.map(function(wordleRanking){
+                                    return <div>
+                                        <ul className = 'user-score'>
+                                            <li>{wordleRanking.rank}</li>
+                                            <li>{wordleRanking.username}</li>
+                                            <li>{parseInt(wordleRanking.rating)}</li>
+                                        </ul>
+                                        <hr/>
+                                    </div>
+                                })}
+                            {userWordleRank !== null && <ul className = 'user-score'>
+                                <li>{userWordleRank.rank}</li>
+                                <li>{userWordleRank.username}</li>
+                                <li>{parseInt(userWordleRank.rating)}</li>
+                            </ul>}
+    
+    
+                                
+                        </div>
+                    </li >
+    
+                    <li className ='anagram-leaderboard'>
+                        
+                        <h1 className='anagram-header'>Anagram</h1>
+                        <hr className="hr"/>
+                        
+    
+                        <div className ='labels'>
+                            <ul>
+                                <li><h3>Rank</h3></li>
+                                <li><h3>Username</h3></li>
+                                <li><h3>Rating</h3></li>
+                            </ul>
+                            {anagramRankings.map(function(anagramRanking){
+                                    return <div>
+                                        <ul className = 'user-score'>
+                                            <li>{anagramRanking.rank}</li>
+                                            <li>{anagramRanking.username}</li>
+                                            <li>{parseInt(anagramRanking.rating)}</li>
+                                        </ul>
+                                        <hr/>
+                                    </div>
                             })}
-                        {userWordleRank !== null && <ul className = 'user-score'>
-                            <li>{userWordleRank.rank}</li>
-                            <li>{userWordleRank.username}</li>
-                            <li>{parseInt(userWordleRank.rating)}</li>
-                        </ul>}
+    
+                            {userAnagramRank !== null && <ul className = 'user-score'>
+                                <li>{userAnagramRank.rank}</li>
+                                <li>{userAnagramRank.username}</li>
+                                <li>{parseInt(userAnagramRank.rating)}</li>
+                            </ul>}
+                           
+                        </div>
+                        
+                    </li>
+    
+                    <li className ='tba-leaderboard'>
+                        
+                        <h1 className='tba-header'>TBA</h1>
+                        <hr className="hr"/>
+                        
+    
+                        <div className ='labels'>
+                            <ul>
+                                <li><h3>Rank</h3></li>
+                                <li><h3>Story Name</h3></li>
+                                <li><h3>Rating</h3></li>
+                            </ul>
+                            {tbaRankings.map(function(tbaRanking){
+                                    return <div>
+                                        <ul className = 'user-score'>
+                                            <li>{tbaRanking.rank}</li>
+                                            <li>{tbaRanking.username}</li>
+                                            <li>{parseInt(tbaRanking.rating)}</li>
+                                        </ul>
+                                        <hr/>
+                                    </div>
+                                })}
+                           
+                        </div>
+                        
+                    </li>
+                </ul>
+               
+              
+            </div>
+            
+    
+        );
+    }
 
-
-                            
-                    </div>
-                </li >
-
-                <li className ='anagram-leaderboard'>
-                    
-                    <h1 className='anagram-header'>Anagram</h1>
-                    <hr className="hr"/>
-                    
-
-                    <div className ='labels'>
-                        <ul>
-                            <li><h3>Rank</h3></li>
-                            <li><h3>Username</h3></li>
-                            <li><h3>Rating</h3></li>
-                        </ul>
-                        {anagramRankings.map(function(anagramRanking){
-                                return <div>
-                                    <ul className = 'user-score'>
-                                        <li>{anagramRanking.rank}</li>
-                                        <li>{anagramRanking.username}</li>
-                                        <li>{parseInt(anagramRanking.rating)}</li>
-                                    </ul>
-                                    <hr/>
-                                </div>
-                        })}
-
-                        {userAnagramRank !== null && <ul className = 'user-score'>
-                            <li>{userAnagramRank.rank}</li>
-                            <li>{userAnagramRank.username}</li>
-                            <li>{parseInt(userAnagramRank.rating)}</li>
-                        </ul>}
-                       
-                    </div>
-                    
-                </li>
-
-                <li className ='tba-leaderboard'>
-                    
-                    <h1 className='tba-header'>TBA</h1>
-                    <hr className="hr"/>
-                    
-
-                    <div className ='labels'>
-                        <ul>
-                            <li><h3>Rank</h3></li>
-                            <li><h3>Story Name</h3></li>
-                            <li><h3>Rating</h3></li>
-                        </ul>
-                        {tbaRankings.map(function(tbaRanking){
-                                return <div>
-                                    <ul className = 'user-score'>
-                                        <li>{tbaRanking.rank}</li>
-                                        <li>{tbaRanking.username}</li>
-                                        <li>{parseInt(tbaRanking.rating)}</li>
-                                    </ul>
-                                    <hr/>
-                                </div>
-                            })}
-                       
-                    </div>
-                    
-                </li>
-            </ul>
-           
-          
-        </div>
-        
-
-    );
+    
 
 }
 
