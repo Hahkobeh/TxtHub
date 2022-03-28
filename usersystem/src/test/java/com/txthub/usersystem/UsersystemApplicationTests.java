68efba96e0f8460be8c5b70b0261b952a1ab2dc1
Matthueh <82128424+Matthueh@users.noreply.github.com>
2022-03-27 23:32:12 -0600

    Add files via upload

--- usersystem/src/test/java/com/txthub/usersystem/UsersystemApplicationTests.java
+++ usersystem/src/test/java/com/txthub/usersystem/UsersystemApplicationTests.java
@@ -1,15 +1,25 @@
 package com.txthub.usersystem;
+import lombok.Data;
+import org.bson.types.ObjectId;
 
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.txthub.usersystem.matchmaking.Challenge;
+import com.txthub.usersystem.matchmaking.ChallengeService;
+import com.txthub.usersystem.matchmaking.ChallengeRepository;
 import com.txthub.usersystem.matchmaking.QueueMember;
 import com.txthub.usersystem.user.User;
 import com.txthub.usersystem.user.UserPair;
+import com.txthub.usersystem.user.UserRepository;
+import com.txthub.usersystem.user.UserService;
 
 @SpringBootTest
 class UsersystemApplicationTests {
@@ -53,12 +63,36 @@ class UsersystemApplicationTests {
 //-------------------------------End of the testing User--------------------------------------------------------//
 
 
-//-------------------------------Testing the UserController-----------------------------------------------------//
-	@Test
-	void UserControllerConstructorTest(){
+//-------------------------------Testing the UserService-----------------------------------------------------//
+	
+	//Don't exactly know how the User Repository is being connected on mongodb should prob ask about this
+	private final UserRepository userRepository;
+	UserService userService = new UserService(userRepository);
 
+	String username = "alexander";
+	String password = "password";
+
+	@Test
+	void UserServiceCreateUserTest(){
+		assertEquals(userService.createUser(username, password) == true, "The user can not be created in user service.");
 	}
-//-------------------------------End of the testing UserController-----------------------------------------------//
+
+	@Test
+	void UserServiceDeleteUserTest(){
+		assertEquals(userService.deleteAccount(username)== true, "The user can not be created in user service");
+	}
+
+	@Test
+	void UserServiceLoginTest(){
+		userService.createUser(username, password);
+		assertTrue(userService.login(username, password) != null, "This user can not be logged into in user service.");
+		userService.deleteAccount(username);
+	}
+
+	// in order to do more tests in this field we need to figure out how to get ID.
+
+
+//-------------------------------End of the testing UserService-----------------------------------------------//
 
 
 //-------------------------------Testing the UserPair------------------------------------------------------------//
@@ -116,5 +150,26 @@ class UsersystemApplicationTests {
 
 //-------------------------------End of the Challenge---------------------------------------------------------------//
 
+//-------------------------------Testing ChallengeService------------------------------------------------------------//
+	//Same problem here, I don't know here theese repositories should equal to???????
+	private final ChallengeRepository chRepo;
+	private final UserRepository userRepo;
+	String challengeId = "0001";
+	String username1 = "alexander";
+	String username2 = "selena";
+	double score1 = 0;
+	double score2 = 1;
+	boolean finished = false;
+	String game = "wordle";
+	
+	@Test
+	void addAndDeleteChallengeTest(){
+		Challenge challenge = new Challenge(challengeId, username1, username2, score1, score2, finished, game);
+		assertEquals(addChallenge(challenge) == true, "The challenge has not been added successfully.");
+		assertEquals(deleteChallenge("0001") == true, "The challenge has not been deleted successfully.");
+	}
+
+
+//-------------------------------End of Service---------------------------------------------------------------------//
 
 }
