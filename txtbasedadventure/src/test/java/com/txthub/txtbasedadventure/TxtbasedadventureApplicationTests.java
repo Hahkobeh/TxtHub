a77b92bb08c2794006bb7d5631d8e5988ead0f3f
Matthueh <82128424+Matthueh@users.noreply.github.com>
2022-03-28 20:42:39 -0600

    Add files via upload

--- txtbasedadventure/src/test/java/com/txthub/txtbasedadventure/TxtbasedadventureApplicationTests.java
+++ txtbasedadventure/src/test/java/com/txthub/txtbasedadventure/TxtbasedadventureApplicationTests.java
@@ -1,11 +1,16 @@
 package com.txthub.txtbasedadventure;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.txthub.txtbasedadventure.node.Node;
 import com.txthub.txtbasedadventure.node.NodeRepository;
+import com.txthub.txtbasedadventure.node.NodeService;
+import com.txthub.txtbasedadventure.story.Genre;
 import com.txthub.txtbasedadventure.story.Story;
+import com.txthub.txtbasedadventure.story.StoryForm;
 import com.txthub.txtbasedadventure.story.StoryRepository;
+import com.txthub.txtbasedadventure.story.StoryService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +22,10 @@ class TxtbasedadventureApplicationTests {
 	void contextLoads() {
 	}
 //---------------------Node Tests--------------------------------//
-
+	NodeRepository nodeRepository;
+	NodeService newNodeService = new NodeService(nodeRepository);
+	StoryRepository storyRepo;
+	StoryService storyService = new StoryService(storyRepo);
 
 	@Test
 	void NodeConstructor1ArgTest(){
@@ -34,11 +42,40 @@ class TxtbasedadventureApplicationTests {
 		assertTrue(node != null, "This node could not be tied to any story.");
 	}
 
+	@Test
+	void CreateandDeleteNodesTest(){
+		String id = "0001";
+		String name = "Gatsby";
+		String authorUsername = "Fitz Gerald";
+		String genre = "MYSTERY";
+		int likes = 0;
+		int dislikes = 0;
 
-//--------------------Node Service--------------------------------//
-	//How does this full connect to the repository?????
-	NodeRepository nodeRepository;
+		Story dud = new Story(id, name, authorUsername, genre, likes, dislikes);
+		storyRepo.save(dud);
 
+		newNodeService.createFirstNode("0002", id);
+		assertTrue(newNodeService.createNode(id) != null, "The node failed to be created.");
+		storyService.deleteStory(id);
+	}
+
+	@Test
+	void getNodesTest(){
+		String id = "0001";
+		String name = "Gatsby";
+		String authorUsername = "Fitz Gerald";
+		String genre = "MYSTERY";
+		int likes = 0;
+		int dislikes = 0;
+
+		Story dud = new Story(id, name, authorUsername, genre, likes, dislikes);
+		storyRepo.save(dud);
+		newNodeService.createFirstNode("0002", id);
+		newNodeService.createNode(id);
+		newNodeService.deleteAllStoryNodes(id);
+		assertTrue( newNodeService.getStoryNodes(id) != null, "The get story nodes failed to work. ");
+		storyService.deleteStory(id);
+	}
 
 //---------------------Story Tests-------------------------------//
 	@Test
@@ -63,13 +100,32 @@ class TxtbasedadventureApplicationTests {
 
 		assertTrue(story != null, "This story has not been created.");
 	}
+//--------------------Story Service Test---------------------------//
 
-//-----------------Story Repository------------------------------//
 
-	StoryRepository storyRepo;
-	
 	@Test
-	void CreateStoryTest(){
+	void getAllStoryTest(){
+		assertTrue(storyService.getAllStories() != null, "All stories can not be obtained.");
+	}
 
+	@Test
+	void getStoriesByLikesTest(){
+		assertTrue(storyService.getStoriesByLikes() != null, "Stories by likes can not be obtained.");
+	}
+
+	@Test
+	void FindAuthorandDeleteStoryTest(){
+
+		String id = "0001";
+		String name = "Gatsby";
+		String authorUsername = "Fitz Gerald";
+		String genre = "MYSTERY";
+		int likes = 0;
+		int dislikes = 0;
+
+		Story dud = new Story(id, name, authorUsername, genre, likes, dislikes);
+		storyRepo.save(dud);
+		storyService.deleteStory(id);
+		assertTrue(storyService.getStoriesByAuthor(authorUsername) == null, "This has not been successfully deleted.");
 	}
 }
